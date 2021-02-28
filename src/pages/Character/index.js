660c16988af3e7e40c6105b21c837f3980e64f15
Miguel <miguelsousalima@hotmail.com>
2021-02-27 21:19:51 -0300

    Settings components Characters and add link Url Marvel

--- src/pages/Character/index.js
+++ src/pages/Character/index.js
@@ -11,7 +11,9 @@ function Character() {
 
   useEffect(() => {
     const handleSearch = () => {
-      setLoading(true);
+      if (character.length === 0) {
+        setLoading(true);
+      }
       readCharacterById(data.state.id, (char) => {
         setCharacter(char);
       });
@@ -19,24 +21,55 @@ function Character() {
       setLoading(false);
     };
     handleSearch();
-  }, [data]);
+  }, [data, character]);
 
   return (
     <div>
       {loading === true ? (
         <h1>Carregando</h1>
       ) : (
-        <div>
+        <>
           <h1>{character.name}</h1>
-          <h3>
-            {character.description
-              ? character.description
-              : 'Não há descrição para esse herói'}
-          </h3>
-          {comics.map((comic) => (
-            <img style={{ width: 100, height: 100 }} src={comic} alt="" />
-          ))}
-        </div>
+          <div style={{ display: 'flex', flexDirection: 'row' }}>
+            <img
+              style={{ width: 200, height: 200 }}
+              src={`${
+                character.thumbnail
+                  ? `${character.thumbnail.path}.${character.thumbnail.extension}`
+                  : null
+              }`}
+              alt={character.name}
+            />
+            <div style={{ display: 'flex', flexDirection: 'column' }}>
+              <p>
+                {character.description
+                  ? character.description
+                  : 'Não há descrição para esse herói'}
+              </p>
+              <ul className="featured-character__links">
+                <div style={{ display: 'flex', flexDirection: 'row' }}>
+                  {character.urls
+                    ? character.urls.map((link) => (
+                        <li
+                          style={{ padding: 20 }}
+                          className="feature-character__link"
+                        >
+                          <a href={link.url}>{link.type}</a>
+                        </li>
+                      ))
+                    : null}
+                </div>
+              </ul>
+            </div>
+          </div>
+
+          <div>
+            <h3>Comics</h3>
+            {comics.map((comic) => (
+              <img style={{ width: 100, height: 100 }} src={comic} alt="" />
+            ))}
+          </div>
+        </>
       )}
     </div>
   );
